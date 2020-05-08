27cb71a30f419c954b4eb6ba2c9a6ad3d56ff52d
Khanyisile <khanyisilemathe@chillisoft.co.za>
2020-05-08 15:54:05 +0200

    wip: add DailyQuestions model

---
+++ src/TeamTeaRexCovid19.Web/Models/DailyQuestions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamTeaRexCovid19.Web.Models
+{
+    public class DailyQuestions
+    {
+    }
+}
