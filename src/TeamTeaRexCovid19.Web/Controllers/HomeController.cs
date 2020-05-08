c4000a9609fef109a532a5e98a300ee7dac4c8af
Braam <braamneethling1991@gmail.com>
2020-05-08 15:24:19 +0200

    Create once off questions page - Implement modal for questions

--- src/TeamTeaRexCovid19.Web/Controllers/HomeController.cs
+++ src/TeamTeaRexCovid19.Web/Controllers/HomeController.cs
@@ -24,6 +24,11 @@ namespace TeamTeaRexCovid19.Web.Controllers
             return View();
         }
 
+        public IActionResult InitialQuestions()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ src/TeamTeaRexCovid19.Web/Models/InitialQuestionsViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TeamTeaRexCovid19.Web.Models
+{
+    public class InitialQuestionsViewModel
+    {
+        [Description("What Province do you currently stay in?")]
+        public string SelectedProvince { get; set; }
+        public IEnumerable<SelectListItem> Provinces { get; set; }
+       
+        [Description("What suburb do you currently stay in?")]
+        public string Suburb { get; set; }
+
+        [Description("What is your Age Group?")]
+        public string SelectedAgeGroup { get; set; }
+        public IEnumerable<SelectListItem> AgeGroups { get; set; }
+
+        [Description("Do you Smoke?")]
+        public bool IsSmoker { get; set; }
+        
+        [Description("Do you drink?")]
+        public bool IsDrinker { get; set; }
+
+        [Description("How many people do you share your living space with?")]
+        public int DoesShareLivingSpace { get; set; }
+
+        [Description("What are you're means of transport?")]
+        public bool TransportType { get; set; }
+        public IEnumerable<SelectListItem> TransportTypes { get; set; }
+    }
+}
