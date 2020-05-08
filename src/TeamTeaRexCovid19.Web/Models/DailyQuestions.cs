c31f5141a56d9b6b9f1956a23c61811f7daf4717
Khanyisile <khanyisilemathe@chillisoft.co.za>
2020-05-08 17:25:49 +0200

    chore : add daily questions

--- src/TeamTeaRexCovid19.Web/Models/DailyQuestions.cs
+++ src/TeamTeaRexCovid19.Web/Models/DailyQuestions.cs
@@ -2,5 +2,13 @@
 {
     public class DailyQuestions
     {
+        public string PeopleInteractedWith { get; set; }
+        public bool Fever { get; set; }
+        public bool PersistentCough { get; set; }
+        public bool UnusualFatigue { get; set; }
+        public bool EatAnything { get; set; }
+        public string FeelRightNow { get; set; }
+        public bool LeaveHomeToday { get; set; }
+        public string Treatment { get; set; }
     }
 }
