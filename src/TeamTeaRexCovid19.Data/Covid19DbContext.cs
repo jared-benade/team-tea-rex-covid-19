14cb047842b7a04ba13dd72b9559322b88a95dc4
Jared Benade <jared.benade@gmail.com>
2020-05-29 08:45:48 +0200

    Add starting point for entity framework

---
+++ src/TeamTeaRexCovid19.Data/Covid19DbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace TeamTeaRexCovid19.Data
+{
+    public class Covid19DbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public Covid19DbContext(IConfiguration configuration, DbContextOptions options) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+            }
+        }
+    }
+}
